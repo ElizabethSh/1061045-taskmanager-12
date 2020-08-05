8eae6cfcceadfdc4a471e20a097751cd91d5003e
ElizabethSh <liza.shoshkina@gmail.com>
2020-08-05 21:49:29 +0200

    Модифицирует функцию отображения задачи: удаляет или добавляет класс в зависимости от данных

--- src/view/task.js
+++ src/view/task.js
@@ -1,18 +1,40 @@
-export const createTaskTemplate = () => {
+import {isTaskExpired, humanizeTaskDueDate, isTaskRepeating} from "../utils.js";
+
+export const createTaskTemplate = (task) => {
+  const {color, description, dueDate, repeating, isFavorite, isArchive} = task;
+
+  // если есть дата дедлайна, приводит ее в человеческий вид
+  const date = (dueDate !== null)
+    ? humanizeTaskDueDate(dueDate) : ``;
+
+  // если есть дедлайн, добавляет соответствующий класс
+  const deadlineClassName = isTaskExpired(dueDate) ? `card--deadline` : ``;
+
+  // если задача повторяемая, добавляет соответствующий класс
+  const repeatClassName = isTaskRepeating(repeating) ? `card--repeat` : ``;
+
+  // если задача в архиве, добавляет disabled на кнопку
+  const arciveClassName = isArchive
+    ? `card__btn--archive card__btn--disabled` : `card__btn--archive`;
+
+  // если задача в избранном, добавляет disabled на кнопку
+  const favoritClassName = isFavorite
+    ? `card__btn--favorites card__btn--disabled` : `card__btn--favorites`;
+
   return (
-    `<article class="card card--black">
+    `<article class="card card--${color} ${deadlineClassName} ${repeatClassName}">
       <div class="card__form">
         <div class="card__inner">
           <div class="card__control">
             <button type="button" class="card__btn card__btn--edit">
               edit
             </button>
-            <button type="button" class="card__btn card__btn--archive">
+            <button type="button" class="card__btn ${arciveClassName}">
               archive
             </button>
             <button
               type="button"
-              class="card__btn card__btn--favorites"
+              class="card__btn ${favoritClassName}"
             >
               favorites
             </button>
@@ -25,7 +47,7 @@ export const createTaskTemplate = () => {
           </div>
 
           <div class="card__textarea-wrap">
-            <p class="card__text">Example task with default color.</p>
+            <p class="card__text">${description}</p>
           </div>
 
           <div class="card__settings">
@@ -33,7 +55,7 @@ export const createTaskTemplate = () => {
               <div class="card__dates">
                 <div class="card__date-deadline">
                   <p class="card__input-deadline-wrap">
-                    <span class="card__date">23 September</span>
+                    <span class="card__date">${date}</span>
                   </p>
                 </div>
               </div>
