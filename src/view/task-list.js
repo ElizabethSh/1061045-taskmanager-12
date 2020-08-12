baee95606793edb3f4359ec37667ff8f42023170
ElizabethSh <liza.shoshkina@gmail.com>
2020-08-12 13:41:51 +0200

    Выносит функцию отображения списка задач в отдельный модуль

---
+++ src/view/task-list.js
@@ -0,0 +1,26 @@
+import {createElement} from "../utils.js";
+
+const createTaskListTemplate = () => {
+  return `<div class="board__tasks"></div>`;
+};
+
+export default class TaskList {
+  constructor() {
+    this._element = null;
+  }
+
+  _getTemplate() {
+    return createTaskListTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this._getTemplate());
+    }
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
