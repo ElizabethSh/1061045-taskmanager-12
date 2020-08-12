3b92ebee8a37030c2c12e82125e695d1395871b5
ElizabethSh <liza.shoshkina@gmail.com>
2020-08-12 13:47:09 +0200

    Добавляет класс LoadButton и экспортирует его

--- src/view/load-button.js
+++ src/view/load-button.js
@@ -1,5 +1,28 @@
-export const loadButtonTemplate = () => {
+import {createElement} from "../utils.js";
+
+const loadButtonTemplate = () => {
   return (
     `<button class="load-more" type="button">load more</button>`
   );
 };
+
+export default class LoadButton {
+  constructor() {
+    this._element = null;
+  }
+
+  _getTemplate() {
+    return loadButtonTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this._getTemplate());
+    }
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
