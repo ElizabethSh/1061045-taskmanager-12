318c357bbd9b74fa837132033574c85540c4f786
ElizabethSh <liza.shoshkina@gmail.com>
2020-08-18 11:00:31 +0200

    Исправляет ошибку в функции remove

--- src/utils/render.js
+++ src/utils/render.js
@@ -59,7 +59,7 @@ export const replace = (newChild, oldChild) => {
 };
 
 export const remove = (component) => {
-  if (!(component === Abstract)) {
+  if (!(component instanceof Abstract)) {
     throw new Error(`Can remove only components`);
   }
 
