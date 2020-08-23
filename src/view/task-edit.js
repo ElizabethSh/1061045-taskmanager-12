06969437fe617e2e5fa966dccaddcda5ef8fe158
ElizabethSh <liza.shoshkina@gmail.com>
2020-08-23 13:45:34 +0200

    Добавляет методы для установки обработчиков на контролы формы редактирования

--- src/view/task-edit.js
+++ src/view/task-edit.js
@@ -1,4 +1,4 @@
-import AbstractView from "./abstract.js";
+import SmartView from "./smart.js";
 import {COLORS} from "../const.js";
 import {isTaskExpired, humanizeTaskDueDate, isTaskRepeating} from "../utils/task.js";
 
@@ -19,31 +19,31 @@ const BLANK_TASK = {
   isFavorite: false
 };
 
-const createTaskEditDateTemplate = (dueDate) => {
+const createTaskEditDateTemplate = (dueDate, isDueDate) => {
   return `<button class="card__date-deadline-toggle" type="button">
-    date: <span class="card__date-status">${dueDate !== null ? `yes` : `no`}</span>
+    date: <span class="card__date-status">${isDueDate ? `yes` : `no`}</span>
   </button>
 
-  ${dueDate !== null ? `<fieldset class="card__date-deadline">
+  ${isDueDate ? `<fieldset class="card__date-deadline">
     <label class="card__input-deadline-wrap">
       <input
         class="card__date"
         type="text"
         placeholder=""
         name="date"
-        value="${humanizeTaskDueDate(dueDate)}"
+        value="${dueDate !== null ? humanizeTaskDueDate(dueDate) : ``}"
       />
     </label>
   </fieldset>` : ``}
   `;
 };
 
-const createTaskEditRepeatingTemplate = (repeating) => {
+const createTaskEditRepeatingTemplate = (repeating, isRepeating) => {
   return `<button class="card__repeat-toggle" type="button">
-      repeat:<span class="card__repeat-status">${isTaskRepeating(repeating) ? `yes` : `no`}</span>
+      repeat:<span class="card__repeat-status">${isRepeating ? `yes` : `no`}</span>
     </button>
 
-    ${isTaskRepeating(repeating) ? `<fieldset class="card__repeat-days">
+    ${isRepeating ? `<fieldset class="card__repeat-days">
       <div class="card__repeat-days-inner">
         ${Object.entries(repeating).map(([day, repeat]) => `<input
           class="visually-hidden card__repeat-day-input"
@@ -76,23 +76,25 @@ const createTaskEditColorsTemplate = (currentColor) => {
     >`).join(``);
 };
 
-const createTaskEditTemplate = (task) => {
+const createTaskEditTemplate = (data) => {
 
-  const {color, description, dueDate, repeating} = task;
+  const {color, description, dueDate, repeating, isDueDate, isRepeating} = data;
 
   // если задача с дедлайном, добавляет класс card--deadline
   const deadlineClassName = isTaskExpired(dueDate)
     ? `card--deadline` : ``;
 
   // получает шаблон в зависимости от ситуации
-  const dateTemplate = createTaskEditDateTemplate(dueDate);
+  const dateTemplate = createTaskEditDateTemplate(dueDate, isDueDate);
 
   // если задача повторяемая, добавляет класс card--repeat
-  const repeatingClassName = isTaskRepeating(repeating)
+  const repeatingClassName = isRepeating
     ? `card--repeat` : ``;
 
-  const repeatingTemplate = createTaskEditRepeatingTemplate(repeating);
+  const repeatingTemplate = createTaskEditRepeatingTemplate(repeating, isRepeating);
   const colorsTemplate = createTaskEditColorsTemplate(color);
+
+  const isSubmitDisabled = isRepeating & !isTaskRepeating(repeating);
 
   return (
     `<article class="card card--edit card--${color} ${repeatingClassName} ${deadlineClassName} ">
@@ -131,7 +133,7 @@ const createTaskEditTemplate = (task) => {
           </div>
 
           <div class="card__status-btns">
-            <button class="card__save" type="submit">save</button>
+            <button class="card__save" type="submit" ${isSubmitDisabled ? `disabled` : ``}>save</button>
             <button class="card__delete" type="button">delete</button>
           </div>
         </div>
@@ -140,24 +142,172 @@ const createTaskEditTemplate = (task) => {
   );
 };
 
-export default class TaskEdit extends AbstractView {
+export default class TaskEdit extends SmartView {
   constructor(task = BLANK_TASK) {
     super();
-    this._task = task;
+    this._data = TaskEdit.parseTaskToData(task);
     this._formSubmitHandler = this._formSubmitHandler.bind(this);
+    this._descriptionInputHahdler = this._descriptionInputHahdler.bind(this);
+    this._dueDateToggleHandler = this._dueDateToggleHandler.bind(this);
+    this._repeatingToggleHandler = this._repeatingToggleHandler.bind(this);
+    this._colorChangeHandler = this._colorChangeHandler.bind(this);
+    this._repeatingChangeHandler = this._repeatingChangeHandler.bind(this);
+
+    // установка обработчиков при создании карточки (первый раз)
+    this._setInnerHandlers();
+  }
+
+  reset(task) {
+    this.updateData(
+        TaskEdit.parseTaskToData(task)
+    );
   }
 
   _getTemplate() {
-    return createTaskEditTemplate(this._task);
+    return createTaskEditTemplate(this._data);
+  }
+
+  // метод восстановления всех обработчиков
+  restoreHandlers() {
+    this._setInnerHandlers();
+    this.setFormSubmitHandler(this._callback.formSubmit);
+  }
+
+  // метод установки внутренних обработчиковб только то
+  // что касается формы редактирования
+  _setInnerHandlers() {
+    // обработчик на кнопку даты
+    this.getElement()
+      .querySelector(`.card__date-deadline-toggle`)
+      .addEventListener(`click`, this._dueDateToggleHandler);
+
+    // обработчик на кнопку повторяемости
+    this.getElement()
+      .querySelector(`.card__repeat-toggle`)
+      .addEventListener(`click`, this._repeatingToggleHandler);
+
+    this.getElement()
+      .querySelector(`.card__text`)
+      .addEventListener(`input`, this._descriptionInputHahdler);
+
+    this.getElement()
+      .querySelector(`.card__colors-wrap`)
+      .addEventListener(`change`, this._colorChangeHandler);
+
+    // навешивать обработчики только если выбрана повторяемость задачи
+    // т.к. если дней повторений в DOM нет, навешивать обработчики
+    // не на что, будет ошибка
+    if (this._data.isRepeating) {
+      this.getElement()
+        .querySelector(`.card__repeat-days-inner`)
+        .addEventListener(`change`, this._repeatingChangeHandler);
+    }
+  }
+
+  _colorChangeHandler(evt) {
+    evt.preventDefault();
+    this.updateData({
+      color: evt.target.value
+    });
+  }
+
+  _repeatingChangeHandler(evt) {
+    evt.preventDefault();
+    this.updateData({
+      repeating: Object.assign(
+          {},
+          this._data.repeating,
+          {[evt.target.value]: evt.target.checked}
+      )
+    });
+  }
+
+  _dueDateToggleHandler(evt) {
+    evt.preventDefault();
+    this.updateData({
+      isDueDate: !this._data.isDueDate,
+
+      // если выбор даты нужно показать,
+      // то есть когда "!this._data.isDueDate === true",
+      // тогда isRepeating должно быть строго false,
+      // что достигается логическим оператором &&
+
+      isRepeating: !this._data.isDueDate && false
+    });
+  }
+
+  _repeatingToggleHandler(evt) {
+    evt.preventDefault();
+    this.updateData({
+      isRepeating: !this._data.isRepeating,
+
+      // если выбор повторяемости нужно показать,
+      // то есть когда "!this._data.isRepeating === true",
+      // тогда isDueDate должно быть строго false,
+      // что достигается логическим оператором &&
+      isDueDate: !this._data.isRepeating && false
+    });
+  }
+
+  _descriptionInputHahdler(evt) {
+    evt.preventDefault();
+    this.updateData({
+      description: evt.target.value
+    }, true);
   }
 
   _formSubmitHandler(evt) {
     evt.preventDefault();
-    this._callback.formSubmit();
+
+    // передаем данные в колбэк, т.к. для формы колбеком будет
+    // выступать метод по обновлению
+    this._callback.formSubmit(TaskEdit.parseDataToTask(this._data));
   }
 
   setFormSubmitHandler(callback) {
     this._callback.formSubmit = callback;
     this.getElement().addEventListener(`submit`, this._formSubmitHandler);
+  }
+
+  // метод берет объект с задачей из мокк и приписывает к нему
+  // 2 флага: isDueDate и isRepeating
+  // в этих флагах с момента init TaskEditComponent будет
+  // храниться признак показывать дату или нет
+  static parseTaskToData(task) {
+    return Object.assign(
+        {},
+        task,
+        {
+          isDueDate: task.dueDate !== null,
+          isRepeating: isTaskRepeating(task.repeating)
+        }
+    );
+  }
+
+  static parseDataToTask(data) {
+    data = Object.assign({}, data);
+
+    // если isDueDate не установлена, установи равной null
+    if (!data.isDueDate) {
+      data.dueDate = null;
+    }
+
+    // если repeating(повторяемость задачи) не установлен, установи все дни false
+    if (!data.isRepeating) {
+      data.repeating = {
+        mo: false,
+        tu: false,
+        we: false,
+        th: false,
+        fr: false,
+        sa: false,
+        su: false
+      };
+    }
+
+    delete data.isDueDate;
+    delete data.isRepeating;
+
+    return data;
   }
 }
