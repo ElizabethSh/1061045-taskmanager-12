1ae5ebcdba900620a41060f3f38a4656af126da6
ElizabethSh <liza.shoshkina@gmail.com>
2020-08-23 13:42:43 +0200

    Добавляет функцию обновления элемента в массиве

--- src/utils/common.js
+++ src/utils/common.js
@@ -5,3 +5,25 @@ export const getRandomInteger = (a = 0, b = 1) => {
 
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
+
+// функция обновления элемента массива
+// (передаем массив и объект, который хотим заменить)
+export const updateItem = (items, update) => {
+  // ищем в массиве этот объект по id,
+  // если есть, вернет индекс элемента
+  const index = items.findIndex((item) => (item.id === update.id));
+
+  // если объекта в массиве нет, верни массив обратно
+  if (index === -1) {
+    return items;
+  }
+
+  // если объект есть, делаем новый массив из части до найденного элемента,
+  // из самого обновленного объекта (update), и из части после
+  // spread-оператор передает элементы массива в виде отдельных элементов (аргументов)
+  return [
+    ...items.slice(0, index),
+    update,
+    ...items.slice(index + 1)
+  ];
+};
