667be01a92d29895cbd03a5389f35b7ead2c6a69
ElizabethSh <liza.shoshkina@gmail.com>
2020-09-01 21:38:29 +0200

    Передает моки в TasksModel, удаляет передачу мокк сразу в BoardPresenter

--- src/main.js
+++ src/main.js
@@ -3,16 +3,20 @@ import FilterView from "./view/filter.js";
 import {generateTask} from "./mock/task.js";
 import {generateFilter} from "./mock/filter.js";
 import BoardPresenter from "./presenter/board.js";
+import TasksModel from "./model/tasks.js";
 import {render, RenderPosition} from "./utils/render.js";
 
 const TASK_COUNT = 22;
 const tasks = new Array(TASK_COUNT).fill().map(generateTask);
 const filters = generateFilter(tasks);
 
+const tasksModel = new TasksModel();
+tasksModel.setTasks(tasks);
+
 const main = document.querySelector(`.main`);
 const siteHeader = main.querySelector(`.main__control`);
 
-const boardPresenter = new BoardPresenter(main);
+const boardPresenter = new BoardPresenter(main, tasksModel);
 
 // отрисовывает меню
 render(siteHeader, new SiteMenuView(), RenderPosition.BEFOREEND);
@@ -21,4 +25,4 @@ render(siteHeader, new SiteMenuView(), RenderPosition.BEFOREEND);
 render(main, new FilterView(filters), RenderPosition.BEFOREEND);
 
 // отрисовывает доску со всем содержимым
-boardPresenter.init(tasks);
+boardPresenter.init();
