f23add45b6dcd1762f631a675deab1a5721df59b
ElizabethSh <liza.shoshkina@gmail.com>
2020-09-02 20:14:30 +0200

    Добавляет обработчик на кнопку Add Task

--- src/main.js
+++ src/main.js
@@ -1,28 +1,35 @@
 import SiteMenuView from "./view/site-menu.js";
-import FilterView from "./view/filter.js";
 import {generateTask} from "./mock/task.js";
-import {generateFilter} from "./mock/filter.js";
 import BoardPresenter from "./presenter/board.js";
+import FilterPresenter from "./presenter/filter.js";
 import TasksModel from "./model/tasks.js";
+import FilterModel from "./model/filter.js";
 import {render, RenderPosition} from "./utils/render.js";
 
 const TASK_COUNT = 22;
 const tasks = new Array(TASK_COUNT).fill().map(generateTask);
-const filters = generateFilter(tasks);
 
 const tasksModel = new TasksModel();
+const filterModel = new FilterModel();
+
 tasksModel.setTasks(tasks);
 
 const main = document.querySelector(`.main`);
 const siteHeader = main.querySelector(`.main__control`);
 
-const boardPresenter = new BoardPresenter(main, tasksModel);
+const boardPresenter = new BoardPresenter(main, tasksModel, filterModel);
+const filterPresenter = new FilterPresenter(main, filterModel, tasksModel);
 
 // отрисовывает меню
 render(siteHeader, new SiteMenuView(), RenderPosition.BEFOREEND);
 
 // отрисовывает фильтры
-render(main, new FilterView(filters), RenderPosition.BEFOREEND);
+filterPresenter.init();
 
 // отрисовывает доску со всем содержимым
 boardPresenter.init();
+
+document.querySelector(`#control__new-task`) .addEventListener(`click`, (evt) => {
+  evt.preventDefault();
+  boardPresenter.createTask();
+});
